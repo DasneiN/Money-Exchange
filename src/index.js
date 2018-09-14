7b534816efd839b33abad7686e1ec45dc263de30
DasneiN <sank.mail@gmail.com>
2018-09-14 21:49:14 +0300

    Clear solution

--- src/index.js
+++ src/index.js
@@ -6,25 +6,6 @@ module.exports = function makeExchange(currency) {
     if (currency <= 0) return {};
     if (currency > 10000) return { error: "You are rich, my friend! We don't have so much coins for exchange" };
 
-    // let result = {},
-    //     designations = {
-    //       H: 50,
-    //       Q: 25,
-    //       D: 10,
-    //       N: 5,
-    //       P: 1
-    //     };
-
-    // for (let key in designations) {
-    //   let value = designations[key];
-
-    //   let n = (currency - currency % value) / value;
-    //   currency = currency - n * value;
-    //   if (n > 0) {
-    //     result[key] = n;
-    //   }
-    // }
-
     let result = {},
         designations = new Map([
       ['H',50],
@@ -43,8 +24,6 @@ module.exports = function makeExchange(currency) {
       }
       
     } );
-
-    
 
     return result;
 }
